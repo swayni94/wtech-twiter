bac22221af3f9d560aceaf928bb0eae9deafc246
wayni <sercanwayni@gmail.com>
2021-05-25 16:07:39 +0300

    follow users component

---
+++ Twitter/Twitter/Controllers/ConnectPeopleController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Twitter.Core.Service;
+using Twitter.Model.Entities;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Twitter.Controllers
+{
+    public class ConnectPeopleController : Controller
+    {
+        private readonly ICoreService<User> userService;
+        private readonly ICoreService<FollowUser> followService;
+
+        public ConnectPeopleController(ICoreService<User> userService, ICoreService<FollowUser> followService)
+        {
+            this.userService = userService;
+            this.followService = followService;
+        }
+
+        public IActionResult Index()
+        {
+            var users = userService.GetDefault(u => u.ID != Guid.Parse(HttpContext.Session.GetString("ID")) && u.Status == Core.Entity.Enum.Status.Active && u.Title != "Admin");
+            var followUsers = followService.GetActive();
+            foreach (var item in followUsers)
+            {
+                users.Remove(userService.GetById(item.FromUserId));
+            }
+            return View(users);
+        }
+
+        public async Task<IActionResult> FollowUser(Guid id)
+        {
+            var connect = new FollowUser();
+            connect.FromUserId = Guid.Parse(HttpContext.Session.GetString("ID"));
+            connect.ToUserId = id;
+            followService.Add(connect);
+            return View();
+        }
+    }
+}
---
+++ Twitter/Twitter/Models/ViewComponents/ConnectPeopleViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Twitter.Core.Service;
+using Twitter.Model.Entities;
+
+namespace Twitter.Models.ViewComponents
+{
+    public class ConnectPeopleViewComponent : ViewComponent
+    {
+        private readonly ICoreService<User> userService;
+
+        public ConnectPeopleViewComponent(ICoreService<User> userService)
+        {
+            this.userService = userService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(userService.GetDefault(u => u.ID != Guid.Parse(HttpContext.Session.GetString("ID")) && u.Status == Core.Entity.Enum.Status.Active && u.Title != "Admin").Take(4).ToList());
+        }
+    }
+}
